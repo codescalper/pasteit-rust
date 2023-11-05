26868cb54590797e184a1059275eed9070403608
codescalper <msfunbook@gmail.com>
2023-11-05 12:17:19 +0530

    Fixed issue

---
+++ client/src/components/ThemeSwitcher.tsx
@@ -0,0 +1,26 @@
+import { Switch } from "@nextui-org/react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeSwitcher() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <Switch
+      defaultSelected={theme === 'dark'} 
+      size="lg"
+      color="secondary"
+      onChange={() => {
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+      }}
+    >
+    </Switch>
+  );
+}
---
+++ client/src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {NextUIProvider} from "@nextui-org/react";
+import {ThemeProvider as NextThemesProvider} from "next-themes";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+  <NextUIProvider>
+  <NextThemesProvider attribute="class" defaultTheme="dark">
+    <main className="purple-dark text-foreground bg-background">
+      
+      <App />
+    </main>
+    </NextThemesProvider>
+  </NextUIProvider>
+</React.StrictMode>,
+)
