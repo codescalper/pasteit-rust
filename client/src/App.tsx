c6277c316050ef7a505bd8c80cb712874500c9bd
codescalper <msfunbook@gmail.com>
2023-11-05 16:25:34 +0530

    Made steps component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Steps from './components/Steps'
 
 function App() {
 
@@ -8,6 +9,9 @@ function App() {
     <>
       <Header />      
       <Hero />  
+      <div className="container mx-auto px-4 lg:w-2/4 my-24">
+      <Steps />
+      </div>
     </>
   )
 }
---
+++ client/src/components/Steps.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@nextui-org/react"
+import { AiOutlineRight } from "react-icons/ai"
+import confetti from 'canvas-confetti';
+
+
+ 
+
+
+const Steps = () => {
+    const handleConfetti = () => {
+        confetti();
+    }  
+  return (
+    <div>
+      <p className="text-base font-semibold leading-7 text-purple-800 dark:text-purple-500 text-center mb-8">
+        Takes 3 steps to share code snippets. No account required.
+      </p>
+      <div className="grid grid-cols-12 justify-center items-center gap-4 text-center mb-12">
+        <div></div>
+        <div className="flex items-center justify-around space-x-2 col-span-4">
+          Paste
+          <AiOutlineRight width={40} />
+        </div>
+        <div className="flex items-center justify-around space-x-2 col-span-4">
+          Create
+          <AiOutlineRight width={40} />
+        </div>
+        <div className="flex items-center justify-around space-x-2">
+          <div className="flex justify-center items-center ">
+            <Button 
+              variant="shadow"
+              className="relative overflow-visible rounded-full hover:-translate-y-1 px-4 xl:px-12 md:px-7 shadow-xl bg-background/30 after:content-[''] after:absolute after:rounded-full after:inset-0 after:bg-background/40 after:z-[-1] after:transition after:!duration-500 hover:after:scale-150 hover:after:opacity-0 "
+              size="sm"
+              onClick={handleConfetti}
+            >Share</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Steps;
