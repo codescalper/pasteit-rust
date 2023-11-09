bf59c45b63688c278e4f196bc313e98bd72d9603
codescalper <msfunbook@gmail.com>
2023-11-09 19:41:19 +0530

    port change

--- server/src/main.rs
+++ server/src/main.rs
@@ -89,7 +89,7 @@ async fn main() -> std::io::Result<()> {
             .route("/submit", web::post().to(submit))
             .route("/paste/{token}", web::get().to(get_paste))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
